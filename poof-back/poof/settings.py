66bdbde4652ccb8057fbab33b6ce8b0fb10d7aec
Chris Georgiades <cgeorgiades27@gmail.com>
2019-12-26 19:42:30 -0500

    Added jwt urls to api.

--- poof-back/poof/settings.py
+++ poof-back/poof/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0ut!rrfrrilnd3tj7t4yb+3lz!9f=z+jpo_^*w(rmvqeua)jij'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0']
 
 
 # Application definition
--- poof-back/poof/urls.py
+++ poof-back/poof/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from fart import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 router = routers.DefaultRouter()
@@ -13,6 +14,9 @@ router.register(r'type', views.TypeViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
 
