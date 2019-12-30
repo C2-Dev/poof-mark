b5aef948edd34994bc837358fdfae419a7a8eca1
z3ke1r <chrisrobertson.gis@gmail.com>
2019-12-30 10:06:42 -0700

    Added angular material and created main toolbar.

--- poof-front/src/app/app.module.ts
+++ poof-front/src/app/app.module.ts
@@ -8,33 +8,49 @@ import { AppComponent } from './app.component';
 import { FartComponent } from './fart/fart.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatSidenavModule,
+         MatToolbarModule,
+         MatIconModule,
+         MatListModule,
+         MatButtonModule,
+         MatMenuModule
+        } from '@angular/material';
 
 export function tokenGetter() {
   return localStorage.getItem('access_token');
 }
 
+// noinspection AngularInvalidImportedOrDeclaredSymbol
 @NgModule({
   declarations: [
     AppComponent,
     FartComponent,
     LoginComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        JwtModule.forRoot({
-            config: {
-                tokenGetter,
-                whitelistedDomains: ['localhost:8000'],
-                blacklistedRoutes: []
-            }
-        }),
-        BrowserAnimationsModule,
-        FormsModule,
-        ReactiveFormsModule,
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        whitelistedDomains: ['localhost:8000'],
+        blacklistedRoutes: []
+      }
+    }),
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
