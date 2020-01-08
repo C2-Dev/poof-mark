6c3d201c84a1aefb43a8b3ac8bd1217a82315e4f
Chris <8351123+z3ke1r@users.noreply.github.com>
2020-01-08 10:33:09 -0700

    Added lat lon to fart model

--- poof-back/fart/models.py
+++ poof-back/fart/models.py
@@ -45,13 +45,15 @@ class Fart(models.Model):
     user = models.ForeignKey(User, related_name='fart', on_delete=models.CASCADE)
     city = models.TextField(blank=True, null=True, default='Flavor Town')
     state = models.TextField(blank=True, null=True, default='BFE')
-    score = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(9999)])
+    score = models.PositiveIntegerField(default=100, validators=[MinValueValidator(1), MaxValueValidator(9999)])
     ftype = models.ForeignKey(FartType, related_name='fartType', on_delete=models.CASCADE)
     noise_scale = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10)])
     smell_scale = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10)])
     exposures = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(100000000)])
     date_farted = models.DateTimeField(default=timezone.now)
     date_entered = models.DateTimeField(auto_now_add=True)
+    lat = models.DecimalField(max_digits=12, decimal_places=8, default=35.255278)
+    lon = models.DecimalField(max_digits=12, decimal_places=8, default=-81.180278)
 
     def __str__(self):
         return str(self.id)
