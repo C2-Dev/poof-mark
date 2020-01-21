1e85818ae13e7e23f01f8ad1baabae9768ee329e
z3ke1r <chrisrobertson.gis@gmail.com>
2020-01-21 13:27:42 -0700

    Add dialog data injection

--- poof-front/src/app/fart-dialog/fart-dialog.component.ts
+++ poof-front/src/app/fart-dialog/fart-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 
 @Component({
   selector: 'app-fart-dialog',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FartDialogComponent implements OnInit {
 
-  constructor() { }
+  fart: any;
+
+  constructor(
+    private dialogRef: MatDialogRef<FartDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data) {
+    this.fart = data.fart;
+  }
 
   ngOnInit() {
   }
